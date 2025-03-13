6d1891c42ec16a430ec6348567bca7f78f86fea9
jongwoo_sohn <jongwoo_sohn@tmaxsoft.co.kr>
2025-03-13 16:13:07 +0900

    apply tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { Button } from "@/components/ui/button"
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="text-3xl bg-blue-500">Tailwind Css 적용 테스트</div>;
+      <div className="text-3xl bg-blue-500">Tailwind Css 적용 테스트</div>
       <Button>Click me</Button>
+      <Button variant="destructive">Click me</Button>
 
     </>
   )
