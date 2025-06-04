fdd569fc2f00cfaa4ff4414079f6f46ac58b435d
jongwoo_sohn <jongwoo_sohn@tmaxsoft.co.kr>
2025-06-04 13:54:08 +0900

    feat: add DevRender

--- src/components/ui/common/ConditionalRender/index.tsx
+++ src/components/ui/common/ConditionalRender/index.tsx
@@ -1,3 +1,7 @@
+import { Typography } from '@mui/material';
+
+import { isDevMode } from '@/utils/env';
+
 export const ConditionalRender = ({
   condition,
   children,
@@ -6,4 +10,13 @@ export const ConditionalRender = ({
   children: React.ReactNode;
 }) => {
   return condition ? <>{children}</> : null;
+};
+
+export const DevRender = ({ children }: { children: React.ReactNode }) => {
+  return isDevMode ? (
+    <div className="border border-gray-800 text-left text-sm p-4 mt-6 mb-6 rounded-lg">
+      <Typography variant="h6">Dev Render</Typography>
+      {children}
+    </div>
+  ) : null;
 };
--- src/pages/error-boundary/index.tsx
+++ src/pages/error-boundary/index.tsx
@@ -6,12 +6,14 @@ import { Box, Typography } from '@mui/material';
 import { Button } from '@tmax/tds';
 import { AnimatePresence, motion } from 'framer-motion';
 
-import { ConditionalRender } from '@/components/ui/common/ConditionalRender';
+import {
+  ConditionalRender,
+  DevRender,
+} from '@/components/ui/common/ConditionalRender';
 import { PageTransition } from '@/components/ui/common/PageTransition';
+import { Spacer } from '@/components/ui/common/Spacer';
 
 import { ErrorBoundaryWrapper } from '@/pages/error-boundary/styled';
-
-import { isDevMode } from '@/utils/env';
 
 const ErrorFallback = ({
   error,
@@ -35,17 +37,12 @@ const ErrorFallback = ({
   }
 
   return (
-    <ErrorBoundaryWrapper>
-      <PageTransition>
-        <Typography variant="h6">Something went wrong</Typography>
-        <Typography
-          sx={{
-            marginBottom: 2,
-          }}
-        >
-          Please click the refresh button
-        </Typography>
-        <Box marginBottom={2}>
+    <PageTransition>
+      <ErrorBoundaryWrapper>
+        <Typography variant="h6">{t('error.generic')}</Typography>
+        <Typography>Please click the refresh button</Typography>
+        <Spacer y={'md'} />
+        <Box>
           <Button
             onClick={() => {
               window.location.reload();
@@ -54,35 +51,22 @@ const ErrorFallback = ({
             Refresh
           </Button>
         </Box>
+        <Spacer y={'md'} />
         <Typography>
           Or you will be automatic refresh in{' '}
           {currentDate > 0 ? currentDate : 0} second
         </Typography>
-        <Typography
-          variant="h6"
-          sx={{
-            marginBottom: 3,
-          }}
-        >
-          Something went wrong
-        </Typography>
-        <p className="text-gray-700 text-lg mb-6">{t('error.generic')}</p>
-        <ConditionalRender condition={isDevMode}>
-          <div className="bg-red-100 border border-red-300 text-left text-sm text-red-800 p-4 mb-6  w-full rounded-lg overflow-auto">
+
+        <DevRender>
+          <Spacer y={'md'} />
+          <div className="bg-red-100 border border-red-300 text-left text-sm text-red-800 p-4 mb-6 rounded-lg overflow-auto">
             <pre>{error.message}</pre>
             <pre className="whitespace-pre-wrap">{error.stack}</pre>
           </div>
-        </ConditionalRender>
-        <div className="flex gap-4">
-          <button
-            onClick={resetErrorBoundary}
-            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-6 py-2 rounded-lg transition"
-          >
-            다시 시도
-          </button>
-        </div>
-      </PageTransition>
-    </ErrorBoundaryWrapper>
+          <Button onClick={resetErrorBoundary}>다시 시도</Button>
+        </DevRender>
+      </ErrorBoundaryWrapper>
+    </PageTransition>
   );
 };
 
