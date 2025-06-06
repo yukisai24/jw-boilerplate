464ca59e96eb640159ca99e2787977bcd49ade48
yukisai24@naver.com <yukisai24@naver.com>
2025-06-07 01:47:59 +0900

    feat: add constants

---
+++ src/constants/common.ts
@@ -0,0 +1,20 @@
+export enum TrueFalseEnum {
+  TRUE = 'true',
+  FALSE = 'false',
+}
+
+export const PREVIOUS_PATH = 'previousPath';
+
+export const extension = [
+  'doc',
+  'docx',
+  'hwp',
+  'ppt',
+  'pptx',
+  'xls',
+  'xlsx',
+  'jpg',
+  'gif',
+  'png',
+  'pdf',
+];
