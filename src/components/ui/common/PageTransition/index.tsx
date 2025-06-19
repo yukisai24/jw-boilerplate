b10d3ae405f5b4d7f5811b59d76606c21145886a
jongwoo_sohn <jongwoo_sohn@tmaxsoft.co.kr>
2025-06-19 18:15:36 +0900

    fix: transition

--- src/components/ui/common/PageTransition/index.tsx
+++ src/components/ui/common/PageTransition/index.tsx
@@ -13,15 +13,17 @@ export const PageTransition = ({ children }: { children: React.ReactNode }) => {
       <motion.div
         layout
         key={location.pathname} // 페이지 경로를 기준으로 재생
-        initial={{ opacity: 0, y: 10 }}
+        initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -10 }}
-        transition={{ duration: 0.2 }}
+        // exit={{ opacity: 0, y: -20 }}
+        transition={{ duration: 0.25, ease: 'easeInOut' }}
         style={{
-          position: 'fixed', // or 'fixed' depending on context
+          position: 'absolute', // or 'fixed' depending on context
           top: 0,
           left: 0,
           width: '100%',
+          minHeight: '100vh', // ✅ height를 명확하게 지정
+          overflow: 'hidden', // ✅ 내부 요소가 튀는 것 방지
         }}
       >
         {children}
--- src/pages/pageNotFound/index.tsx
+++ src/pages/pageNotFound/index.tsx
@@ -1,14 +1,14 @@
+import { Suspense } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { Stack, Typography } from '@mui/material';
-import { Button, Icon } from '@tmax/tds';
+import { Button, Icon, Loading } from '@tmax/tds';
 
 import { PageTransition } from '@/components/ui/common/PageTransition';
 
 import { PageNotFoundContainer } from '@/pages/pageNotFound/styled';
 
 import { paths } from '@/routes/paths';
-import theme from '@/theme/index';
 
 const PageNotFound = () => {
   const navigate = useNavigate();
@@ -18,24 +18,24 @@ const PageNotFound = () => {
   };
 
   return (
-    <PageTransition>
-      <PageNotFoundContainer>
-        <Stack
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-          height="100%"
-          spacing={7}
-        >
-          <Icon name="tm_404_error" />
-
-          <Button onClick={navigateToHomePage}>Go back</Button>
-        </Stack>
-        <Stack justifyContent="center">
-          Copyrightⓒ 2023. TmaxSoft, All Rights Reserved.
-        </Stack>
-      </PageNotFoundContainer>
-    </PageTransition>
+    <PageNotFoundContainer>
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        height="100%"
+        spacing={7}
+      >
+        <Icon
+          name="tm_404_error"
+          fallback={<Loading fullpage />}
+        />
+        <Button onClick={navigateToHomePage}>Go back</Button>
+      </Stack>
+      <Stack justifyContent="center">
+        Copyrightⓒ 2023. TmaxSoft, All Rights Reserved.
+      </Stack>
+    </PageNotFoundContainer>
   );
 };
 
